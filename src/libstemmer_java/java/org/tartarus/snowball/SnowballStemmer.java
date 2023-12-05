3e1f98d0c6fb95e121bbbafa4838495366ede276
janellybean <janellechan37@yahoo.com>
2023-12-04 22:58:20 -0800

    COMPLETEORIGINAL

--- src/libstemmer_java/java/org/tartarus/snowball/SnowballStemmer.java
+++
@@ -1,7 +0,0 @@
-
-package libstemmer_java.java.org.tartarus.snowball;
-import java.lang.reflect.InvocationTargetException;
-
-public abstract class SnowballStemmer extends SnowballProgram {
-    public abstract boolean stem();
-};
--- src/methods/documents/FileDocumentFactory.java
+++
@@ -1,7 +0,0 @@
-package methods.documents;
-
-import java.nio.file.Path;
-
-public interface FileDocumentFactory {
-	FileDocument createFileDocument(Path absoluteFilePath, int documentId);
-}
